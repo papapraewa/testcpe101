4329a2b80062820cf19740575276ae68267a2536
Parewa <Parewa.p24@gmail.com>
2022-11-30 21:20:15 +0700

    First Commit

---
+++ a+b.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b,c;
+	printf("Enter A: ");
+	scanf("%d",&a);
+	printf("Enter B: ");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Total = %d ",c);
+	getch();
+	return 0;
+}
